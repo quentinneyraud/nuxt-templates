82da2818dd73b4e3a6fee768b24507a6f9a79269
Quentin Neyraud <quentin.neyraud@gmail.com>
2021-01-04 16:03:21 +0100

    prismic image initial commit

---
+++ components/AppImage.vue
@@ -0,0 +1,236 @@
+<template>
+  <!-- <ViewportObserver
+    v-bind="viewportObserverOptions"
+    :active="lazyload !== 'called'"
+    @enter="!loaded && load()"
+  > -->
+  <div
+    class="AppImage-component"
+    :class="{
+      loaded,
+      lazyload,
+      'has-ratio': !!ratio,
+      'has-not-src': !src
+    }"
+    :style="background"
+  >
+    <div
+      class="AppImage-ratioHelper"
+      :style="ratio ? {
+        paddingTop: ratio * 100 + '%'
+      } : null"
+    >
+      <picture>
+
+        <source
+          v-for="(source, sourceIndex) in internalSources"
+          :key="sourceIndex"
+          ref="sources"
+          v-bind="source"
+        >
+
+        <!-- Image -->
+        <img
+          v-if="!!src"
+          ref="image"
+          :data-src="src"
+          :alt="alt"
+          :style="{
+            objectFit: fit,
+            objectPosition: position
+          }"
+          class="AppImage-image"
+        >
+      </picture>
+    </div>
+  </div>
+  <!-- </ViewportObserver> -->
+</template>
+
+<script>
+export default {
+  name: 'AppImage',
+  props: {
+    src: {
+      type: String,
+      required: false,
+      default: 'https://source.unsplash.com/600x300/?nature,water'
+    },
+    sources: {
+      type: Array,
+      required: false,
+      default: _ => []
+    },
+    alt: {
+      type: String,
+      required: false,
+      default: 'Image non disponible'
+    },
+    fit: {
+      type: String,
+      required: false,
+      default: 'cover',
+      validator: value => ['cover', 'contain', 'none'].includes(value)
+    },
+    position: {
+      type: String,
+      required: false,
+      default: 'center center'
+    },
+    ratio: {
+      type: Number,
+      required: false,
+      default: null
+    },
+    lazyload: {
+      type: [Boolean, String],
+      required: false,
+      default: true
+    },
+    lazyloadOffset: {
+      type: Number,
+      required: false,
+      default: 500
+    },
+    viewportObserverOptions: {
+      type: Object,
+      required: false,
+      default: null
+    },
+    placeholder: {
+      type: String,
+      required: false,
+      default: 'color'
+    }
+  },
+  data () {
+    return {
+      loaded: false,
+      loading: false,
+      error: false,
+      internalSources: [],
+      background: null
+    }
+  },
+  mounted () {
+    this.setSources()
+    this.setBackground()
+
+    if (this.src && !this.lazyload) {
+      this.$nextTick(this.load)
+    }
+
+    window.setTimeout(_ => {
+      this.load()
+    }, 1500)
+  },
+  methods: {
+    async setBackground () {
+      if (this.placeholder === 'blur') {
+        this.background = {
+          backgroundImage: `url(${ this.src + '&blur=300' })`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center center'
+        }
+      } else if (this.placeholder === 'color') {
+        const palette = await (await fetch(this.src + '&palette=json&colors=1')).json()
+
+        this.background = {
+          backgroundColor: palette.dominant_colors.muted.hex
+        }
+      }
+    },
+    setSources () {
+      this.internalSources = this.sources.map(({ src, media, sizes }) => {
+        const cleanSource = {
+          'data-srcset': src?.srcSet || src
+        }
+
+        if (media) {
+          cleanSource.media = media
+        }
+
+        if (sizes) {
+          cleanSource.sizes = sizes
+        }
+
+        return cleanSource
+      })
+
+      // Push default source
+      this.internalSources.push({
+        'data-srcset': this.src
+      })
+    },
+    load () {
+      if (this.loaded) return null
+
+      this.loading = true
+
+      if (!this.$refs.image) {
+        this.onError()
+        return
+      }
+
+      return new Promise((resolve, reject) => {
+        this.$refs.image.addEventListener('load', _ => {
+          this.onLoad()
+          resolve()
+        })
+
+        this.$refs.image.addEventListener('error', err => {
+          this.onError()
+          reject(err)
+        })
+
+        if (this.$refs.image) {
+          this.$refs.image.src = this.$refs.image.dataset.src
+        }
+
+        this.$refs.sources
+          .forEach(source => {
+            source.srcset = source.dataset.srcset
+          })
+      })
+    },
+    onLoad () {
+      this.loaded = true
+      this.loading = false
+      this.$emit('loaded')
+    },
+    onError () {
+      this.loaded = false
+      this.loading = false
+      this.error = true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.AppImage-component {
+  overflow: hidden;
+}
+.AppImage-component.loaded .AppImage-image {
+  opacity: 1;
+  transition: opacity 0.3s;
+}
+.AppImage-component.has-not-src {
+  background-color: #000;
+}
+.AppImage-component.has-ratio .AppImage-ratioHelper {
+  height: 0;
+}
+.AppImage-component:not(.has-ratio) .AppImage-ratioHelper {
+  height: 100%;
+}
+.AppImage-ratioHelper {
+  position: relative;
+  width: 100%;
+}
+.AppImage-image {
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,33 @@
 <template>
-  <div />
+  <div>
+    <AppImage
+      class="test"
+      :src="main_image.url"
+    />
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      main_image: {
+        dimensions: {
+          width: 4118,
+          height: 3089
+        },
+        alt: null,
+        copyright: null,
+        url: 'https://images.prismic.io/jacquesrival/a9137758-207a-475a-9092-db5f94168de0_P1013385.jpg?auto=compress,format'
+      }
+    }
+  }
+}
 </script>
 
-<style>
+<style scoped>
+.test {
+  width: 250px;
+  height: 500px;
+}
 </style>
