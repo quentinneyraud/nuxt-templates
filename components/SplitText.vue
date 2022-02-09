0df7926914c8c56deccb3c84823e9f9e70df1baf
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-02-09 18:11:59 +0100

    Split text WIP

--- components/SplitText.vue
+++ components/SplitText.vue
@@ -21,64 +21,93 @@ export default {
     }
   },
   mounted () {
-    console.clear()
-    this.createLastLine()
-    this.splitAll()
+    if (module.hot) {
+      module.hot.decline()
+    }
+    // this.createLastLine()
+    // this.splitAll()
+
+    console.log(this.method2(this.$refs.helper).slice(0, 50))
   },
   methods: {
-    createLastLine () {
-      this.lastLine = document.createElement('div')
-      this.lastLine.classList.add('line')
-      this.$refs.fake.appendChild(this.lastLine)
-    },
-    splitAll () {
-      this.tree = this.getChildren(this.$refs.helper)
-    },
-    getChildren (el, parentTag) {
-      let currentTag = null
+    // createLastLine () {
+    //   this.lastLine = document.createElement('div')
+    //   this.lastLine.classList.add('line')
+    //   this.$refs.fake.appendChild(this.lastLine)
+    // },
+    // splitAll () {
+    //   this.tree = this.getChildren(this.$refs.helper)
+    // },
+    // getChildren (el, parentTag) {
+    //   let currentTag = null
 
-      if (el !== this.$refs.helper) {
-        if (el.nodeType === 3) {
-          const words = el.textContent.split(' ').filter(v => !!v)
+    //   if (el !== this.$refs.helper) {
+    //     if (el.nodeType === 3) {
+    //       const words = el.textContent.split(' ').filter(v => !!v)
 
-          words.forEach(w => {
-            const wordElement = document.createElement('div')
-            wordElement.classList.add('word')
-            wordElement.innerHTML = w
+    //       words.forEach(w => {
+    //         const wordElement = document.createElement('div')
+    //         wordElement.classList.add('word')
+    //         wordElement.innerHTML = w
 
-            parentTag.append(' ')
-            parentTag.append(wordElement)
+    //         parentTag.append(' ')
+    //         parentTag.append(wordElement)
+    //         console.log('AJOUT ' + w, parentTag.tagName, parentTag.parentNode.tagName)
 
-            if (
-              this.lastLine.getBoundingClientRect().width >
-              this.$el.getBoundingClientRect().width
-            ) {
-              this.createLastLine()
+    //         if (
+    //           this.lastLine.getBoundingClientRect().width >
+    //           this.$el.getBoundingClientRect().width
+    //         ) {
+    //           console.log('Trop grand, nouvelle ligne')
+    //           this.createLastLine()
 
-              parentTag = parentTag.cloneNode(true)
-              parentTag.innerHTML = ''
-              this.lastLine.appendChild(parentTag)
-              parentTag.append(' ')
-              parentTag.append(wordElement)
-            }
-          })
-        } else {
-          currentTag = document.createElement(el.tagName)
+    //           parentTag = parentTag.cloneNode(true)
+    //           parentTag.innerHTML = ''
+    //           console.log('New parentTag', parentTag)
+    //           this.lastLine.appendChild(parentTag)
+    //           parentTag.append(' ')
+    //           parentTag.append(wordElement)
+    //         }
+    //       })
+    //     } else {
+    //       currentTag = document.createElement(el.tagName)
 
-          if (parentTag) {
-            parentTag.appendChild(currentTag)
+    //       if (parentTag) {
+    //         parentTag.appendChild(currentTag)
+    //       } else {
+    //         this.lastLine.appendChild(currentTag)
+    //       }
+    //     }
+
+    //     // if (el.tagName === 'BR') {
+    //     //   this.createLastLine()
+    //     // }
+    //   }
+
+    //   Array.from(el.childNodes)
+    //     .forEach(a => this.getChildren(a, currentTag))
+    // },
+    method2 (el, baseAcc = []) {
+      return Array.from(el.childNodes)
+        .reduce((acc, child) => {
+          if (child.nodeType === 3) {
+            const words = child.textContent
+              .split(' ')
+              .filter(v => !!v)
+              .map(w => {
+                return {
+                  w,
+                  parent: child
+                }
+              })
+
+            acc.push(...words)
           } else {
-            this.lastLine.appendChild(currentTag)
+            this.method2(child, acc)
           }
-        }
 
-        if (el.tagName === 'BR') {
-          this.createLastLine()
-        }
-      }
-
-      Array.from(el.childNodes)
-        .forEach(a => this.getChildren(a, currentTag))
+          return acc
+        }, baseAcc)
     }
   }
 }
