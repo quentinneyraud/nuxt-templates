ef874ca6cecf19ffb2b540e6a60928fe9fcf3cbd
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-02-21 13:39:07 +0100

    Add nuxt-templates CLI config

---
+++ nuxt-templates-cli.js
@@ -0,0 +1,13 @@
+module.exports = {
+  metas: {
+    title: 'Virtual scroll',
+    description: 'Add virtual scroll'
+  },
+  dependencies: [
+    'virtual-scroll'
+  ],
+  devDependencies: [],
+  files: [
+    'modules/virtual-scroll'
+  ]
+}
