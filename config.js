f43de2c7d62a6327daceb29f66c7c34b3cb5fc78
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-03-09 17:57:33 +0100

    Config

--- config.js
+++ config.js
@@ -1,16 +1,5 @@
 module.exports = _ => ({
   modules: [
     '~/modules/audio'
-  ],
-  device: {
-    defaultwindowWidth: 1440,
-    breakpoints: {
-      small: 576,
-      medium: 768,
-      large: 992,
-      xlarge: 1200,
-      xxlarge: 1440,
-      xxxlarge: 1920
-    }
-  }
+  ]
 })
