8526dbf08157249954cd332b9b2228d9828446e0
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-03-09 18:37:44 +0100

    Config

--- config.js
+++ config.js
@@ -4,21 +4,21 @@ module.exports = _ => ({
   ],
   device: {
     /**
-     * Simulated window width in SSR mode (Default: 1440)
+      Simulated window width in SSR mode (Default: 1440)
      */
-    // defaultWindowWidth: 1440,
+    defaultWindowWidth: 1440,
 
     /**
-     * List of breakpoints where key is the name of the breakpoint and value is the window width
-     * Key is used to create plugins keys ('tablet' will create 'gtTablet', 'gteTablet', 'ltTablet', 'lteTablet')
+      List of breakpoints where key is the name of the breakpoint and value is the window width
+      Key is used to create plugins keys ('tablet' will create 'gtTablet', 'gteTablet', 'ltTablet', 'lteTablet')
      */
-    // breakpoints: {
-    //   small: 576,
-    //   medium: 768,
-    //   large: 992,
-    //   xlarge: 1200,
-    //   xxlarge: 1440,
-    //   xxxlarge: 1920
-    // }
+    breakpoints: {
+      small: 576,
+      medium: 768,
+      large: 992,
+      xlarge: 1200,
+      xxlarge: 1440,
+      xxxlarge: 1920
+    }
   }
 })
