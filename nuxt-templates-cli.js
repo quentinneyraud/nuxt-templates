a0f0ee7064c38d69e247dd56ffa9ab712cd35f13
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-03-09 18:46:16 +0100

    nuxt-templates-cli config

---
+++ nuxt-templates-cli.js
@@ -0,0 +1,12 @@
+module.exports = {
+  metas: {
+    title: 'Prismic image',
+    description: 'Add AppImage component'
+  },
+  dependencies: [],
+  devDependencies: [],
+  files: [
+    'components/AppImage.vue',
+    'components/AppSource.vue'
+  ]
+}
