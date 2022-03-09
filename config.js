7da7f9a13d5ac63a4cbf4d8e18d8e804fee49d77
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-03-09 18:41:36 +0100

    Comments

--- config.js
+++ config.js
@@ -3,6 +3,16 @@ module.exports = _ => ({
     '~/modules/events'
   ],
   events: {
+    /**
+      Array of event names that will be used (Default: [])
+      Availbale events:
+        - resize
+        - resize-debounced: resize debounced of 300ms
+        - tick: use gsap if installed and fallback to requestAnimationFrame otherwise
+        - visibility
+        - orientation
+        - mousemove
+     */
     events: []
   }
 })
