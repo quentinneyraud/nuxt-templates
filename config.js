e69371d8976d5c99bd80c3c52aba486869dc27c0
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-03-09 16:30:36 +0100

    Add nuxt-templates-cli config

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = _ => ({
+  plugins: [
+    '~/plugins/api'
+  ],
+})
---
+++ nuxt-templates-cli.js
@@ -0,0 +1,11 @@
+module.exports = {
+  metas: {
+    title: 'Prismic API',
+    description: 'Add a plugin to query data from prismic and format them'
+  },
+  dependencies: [],
+  devDependencies: [],
+  files: [
+    'plugins/api'
+  ]
+}
