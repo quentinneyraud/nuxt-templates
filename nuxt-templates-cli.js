c3937f82a062729c8cfd6ed789832b910c1a9bfb
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-03-10 15:09:25 +0100

    nuxt-templates-cli config

---
+++ nuxt-templates-cli.js
@@ -0,0 +1,13 @@
+module.exports = {
+  metas: {
+    title: 'Vimeo player',
+    description: 'Add a VimeoPlayer component'
+  },
+  dependencies: [
+    '@vimeo/player'
+  ],
+  devDependencies: [],
+  files: [
+    'components/VimeoPlayer.vue'
+  ]
+}
