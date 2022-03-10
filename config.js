c4af294903fc422af158b3284b28cf51e6af70c5
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-03-10 14:12:23 +0100

    nuxt-templates-cli config

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = _ => ({
+  buildModules: [
+    '@nuxtjs/svg'
+  ]
+})
---
+++ nuxt-templates-cli.js
@@ -0,0 +1,15 @@
+module.exports = {
+  metas: {
+    title: 'SVG',
+    description: 'Add SVG component'
+  },
+  dependencies: [],
+  devDependencies: [
+    'svgo',
+    '@nuxtjs/svg'
+  ],
+  files: [
+    'components/AppSvg.vue',
+    'svgo.config.js'
+  ]
+}
