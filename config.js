99d856ad908d000a4b92616167f2c6f6fb60331d
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-03-14 23:51:38 +0100

    Add doc URL in comments

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ module.exports = ({ BASE_URL } = {}) => ({
   modules: [
     '@nuxtjs/sitemap'
   ],
+  // https://sitemap.nuxtjs.org/fr/guide/configuration
   sitemap: {
     hostname: BASE_URL
   }
