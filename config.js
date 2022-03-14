b0ae66475c6062aaff5ae40819ff57186bc86257
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-03-14 23:48:52 +0100

    Add nuxt-templates-cli configs

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = _ => ({
+  buildModules: [
+    '@aceforth/nuxt-netlify'
+  ],
+  // https://github.com/juliomrqz/nuxt-netlify#usage
+  netlify: {}
+})
---
+++ nuxt-templates-cli.js
@@ -0,0 +1,11 @@
+module.exports = {
+  metas: {
+    title: 'Netlify',
+    description: 'Generate _headers and _redirects files for Netlify'
+  },
+  dependencies: [],
+  devDependencies: [
+    '@aceforth/nuxt-netlify'
+  ],
+  files: []
+}
