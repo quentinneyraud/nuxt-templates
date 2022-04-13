89841b5b9d3ba4120eaf688cb560182076891edb
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-04-13 17:49:15 +0200

    Add feature category

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -1,7 +1,8 @@
 module.exports = {
   metas: {
     title: 'Debug',
-    description: 'Add helpers to show grid and images without alt attributes'
+    description: 'Add helpers to show grid and images without alt attributes',
+    category: 'Helpers'
   },
   dependencies: [],
   devDependencies: [],
