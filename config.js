b239280cbb6e7ccfd1ea0a5bfe07a9237e598d03
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-04-13 17:03:14 +0200

    Create Prismic module

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = ({ IS_DEV, IS_PREPROD }) => ({
+  buildModules: [
+    '@nuxtjs/prismic'
+  ],
+  prismic: {
+    endpoint: 'https://my-site.cdn.prismic.io/api/v2',
+    modern: !IS_DEV,
+    preview: IS_PREPROD ? '/preview/' : false
+  }
+})
---
+++ nuxt-templates-cli.js
@@ -0,0 +1,11 @@
+module.exports = {
+  metas: {
+    title: 'Prismic',
+    description: 'Add Prismic module for Nuxt'
+  },
+  dependencies: [],
+  devDependencies: [
+    '@nuxtjs/prismic'
+  ],
+  files: []
+}
