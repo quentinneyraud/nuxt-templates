c236eca24ad43b669267c5d90ad616ae483e0631
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-04-13 17:58:11 +0200

    Add feature category

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -1,7 +1,8 @@
 module.exports = {
   metas: {
     title: 'Prismic',
-    description: 'Add Prismic module for Nuxt'
+    description: 'Add Prismic module for Nuxt',
+    category: 'CMS'
   },
   dependencies: [],
   devDependencies: [
