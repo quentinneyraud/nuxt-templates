f0b09241474412c0d8dab1a85f47bdc6bc3da82f
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-04-13 17:48:31 +0200

    Add feature category

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -1,7 +1,8 @@
 module.exports = {
   metas: {
     title: 'Audio',
-    description: 'Add a plugin and a directive to play sounds'
+    description: 'Add a plugin and a directive to play sounds',
+    category: 'Components'
   },
   dependencies: [],
   devDependencies: [],
