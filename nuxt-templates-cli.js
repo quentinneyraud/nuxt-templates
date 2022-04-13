1d92d28d9f149028f2cf25c5c99d4d179a460a95
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-04-13 17:54:57 +0200

    Add feature category

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -1,7 +1,8 @@
 module.exports = {
   metas: {
     title: 'Netlify',
-    description: 'Generate _headers and _redirects files for Netlify'
+    description: 'Generate _headers and _redirects files for Netlify',
+    category: 'Hosting / Deployment'
   },
   dependencies: [],
   devDependencies: [
