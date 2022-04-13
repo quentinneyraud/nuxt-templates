5df13d668cdf15b8dc741821c321e067f7d5179c
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-04-13 18:06:33 +0200

    Add feature category

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -1,7 +1,8 @@
 module.exports = {
   metas: {
     title: 'Screen based animation',
-    description: 'Add a directive to get elements positions in viewport'
+    description: 'Add a directive to get elements positions in viewport',
+    category: 'Animations'
   },
   dependencies: [],
   devDependencies: [],
