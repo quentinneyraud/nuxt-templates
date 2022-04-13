595e333cdc4c97b436d32488dd22933c64c33cb0
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-04-13 18:02:24 +0200

    Add feature category

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -1,7 +1,8 @@
 module.exports = {
   metas: {
     title: 'RGPD',
-    description: 'Add RgpdBanner and RgpdPopup components and a plugin to manage RGPD services'
+    description: 'Add RgpdBanner and RgpdPopup components and a plugin to manage RGPD services',
+    category: 'Components'
   },
   dependencies: [
     'js-cookie'
