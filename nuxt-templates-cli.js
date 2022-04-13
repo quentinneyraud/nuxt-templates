41fba57440cfdc9a33f71c4f3c385d169e900163
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-04-13 19:46:46 +0200

    Add feature category

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -1,7 +1,8 @@
 module.exports = {
   metas: {
     title: 'Pixel perfect',
-    description: 'Add a component to display mockup on top of the page'
+    description: 'Add a component to display mockup on top of the page',
+    category: 'Development helpers'
   },
   dependencies: [],
   devDependencies: [],
