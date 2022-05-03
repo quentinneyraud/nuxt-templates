72b15f80302759ff5d2c95ccfffeef830cb164ae
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-05-03 17:54:03 +0200

    Fix missing module config

--- config.js
+++ config.js
@@ -2,6 +2,9 @@ module.exports = ({ IS_DEV, IS_PREPROD }) => ({
   buildModules: [
     '@nuxtjs/prismic'
   ],
+  modules: [
+    '~/modules/prismic'
+  ],
   prismic: {
     endpoint: 'https://my-site.cdn.prismic.io/api/v2',
     modern: !IS_DEV,
