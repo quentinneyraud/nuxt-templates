b7581a3478e1c40175589b113e83599ef49e5e94
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-05-04 14:05:14 +0200

    Create PWA feature

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = ({ title = 'My company', description = 'My company is doing something', lang = 'en', themeColor = '#FFFFFF' }) => ({
+  modules: [
+    '@nuxtjs/pwa'
+  ],
+
+  pwa: {
+    // https://pwa.nuxtjs.org/icon/
+    icon: {
+      plugin: false
+    },
+    // https://pwa.nuxtjs.org/manifest
+    manifest: {
+      name: title,
+      short_name: title,
+      lang,
+      description,
+      background_color: themeColor,
+      theme_color: themeColor
+    }
+  }
+})
---
+++ nuxt-templates-cli.js
@@ -0,0 +1,12 @@
+module.exports = {
+  metas: {
+    title: 'PWA',
+    description: 'Add the config to be PWA compatible',
+    category: 'Hosting / Deployment'
+  },
+  dependencies: [
+    '@nuxtjs/pwa'
+  ],
+  devDependencies: [],
+  files: []
+}
