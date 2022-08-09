c88a527419664454ba45edcda5d435a9b0922a9a
Quentin Neyraud <quentin.neyraud@gmail.com>
2022-08-09 16:29:41 +0200

    Create getMetas method to allow override

--- modules/metas/mixins/hasMetas.js
+++ modules/metas/mixins/hasMetas.js
@@ -2,18 +2,23 @@ import constructMetas from '../constructMetas'
 
 export default {
   head () {
-    if (!this.metas) return
+    return this.getMetas()
+  },
+  methods: {
+    getMetas () {
+      if (!this.metas) return
 
-    const title = this.metas.title
-    const description = this.metas.description
-    const url = this.$config.BASE_URL ? this.$config.BASE_URL + this.$route.fullPath : null
-    const image = this.metas.image
+      const title = this.metas.title
+      const description = this.metas.description
+      const url = this.$config.BASE_URL ? this.$config.BASE_URL + this.$route.fullPath : null
+      const image = this.metas.image
 
-    return constructMetas({
-      title,
-      description,
-      url,
-      image
-    })
+      return constructMetas({
+        title,
+        description,
+        url,
+        image
+      })
+    }
   }
 }
