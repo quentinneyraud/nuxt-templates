fc2bbe020d9146c477e9ee9c70c32fd3c8afebf8
Quentin Neyraud <quentin.neyraud@gmail.com>
2023-01-26 19:40:32 +0100

    Sticky exampe

--- pages/index.vue
+++ pages/index.vue
@@ -31,6 +31,22 @@
     </div>
 
     <section
+      v-vs-section
+      :style="{
+        backgroundColor: `#FFF`
+      }"
+    >
+      <div class="stickyContainer">
+        <div
+          v-vs-sticky="{
+            onProgress
+          }"
+          class="stickyChild"
+        />
+      </div>
+    </section>
+
+    <section
       v-for="index in 10"
       :key="index"
       v-vs-section
@@ -40,6 +56,7 @@
     >
       <p>{{ index }}</p>
     </section>
+
   </div>
 </template>
 
@@ -49,6 +66,11 @@ export default {
     this.$virtualScroll.$on('scroll', opts => {
       // console.log('opts:', opts)
     })
+  },
+  methods: {
+    onProgress (progress) {
+      console.log('progress:', progress)
+    }
   }
 }
 </script>
@@ -73,5 +95,25 @@ p {
 
 .actions button:nth-child(n + 2) {
   margin-left: 10px;
+}
+
+.stickyContainer {
+  position: absolute;
+  top: 25vh;
+  left: 50%;
+  width: 500px;
+  height: 50vh;
+  transform: translateX(-50%);
+  border: 1px solid blue;
+  box-sizing: border-box;
+}
+
+.stickyChild {
+  position: absolute;
+  left: 50%;
+  width: 150px;
+  height: 50px;
+  background-color: red;
+  box-sizing: border-box;
 }
 </style>
