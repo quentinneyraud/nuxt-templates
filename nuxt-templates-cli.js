6d356444cf4c30e493ff8a396e8eec1ed0bad837
Quentin Neyraud <quentin.neyraud@gmail.com>
2023-03-15 11:18:23 +0100

    Create Eslint feature

---
+++ nuxt-templates-cli.js
@@ -0,0 +1,14 @@
+module.exports = {
+  metas: {
+    title: 'Eslint',
+    description: 'Add eslint config',
+    category: 'Development helpers'
+  },
+  dependencies: [],
+  devDependencies: [
+    '@babel/eslint-parser'
+  ],
+  files: [
+    '.eslintrc.js'
+  ]
+}
