44d7dcb5458f1e1a0fdb89afed115f983f00e897
Quentin Neyraud <quentin.neyraud@gmail.com>
2023-04-03 15:41:36 +0200

    Lint

--- nuxt-templates-cli.js
+++ nuxt-templates-cli.js
@@ -4,9 +4,7 @@ module.exports = {
     description: 'Add a favicon',
     category: 'Hosting / Deployment'
   },
-  dependencies: [
-    'nuxt-rfg-icon'
-  ],
+  dependencies: ['nuxt-rfg-icon'],
   devDependencies: [],
   files: []
 }
