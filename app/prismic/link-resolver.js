0b23228d7a699306da987016ae42e039ee5b4e11
Quentin Neyraud <quentin.neyraud@gmail.com>
2023-04-03 14:52:44 +0200

    Fix #68 - Use routes resolver

--- app/prismic/link-resolver.js
+++ app/prismic/link-resolver.js
@@ -1,13 +1,5 @@
 export default doc => {
   if (doc.isBroken) return '/not-found'
 
-  if (doc.type === 'about_page') {
-    return '/about'
-  }
-
-  if (doc.type === 'project') {
-    return `/projects/${doc.uid}`
-  }
-
-  return '/not-found'
+  return null
 }
--- config.js
+++ config.js
@@ -2,12 +2,27 @@ module.exports = ({ IS_DEV, IS_PREPROD }) => ({
   buildModules: [
     '@nuxtjs/prismic'
   ],
+
   modules: [
     '~/modules/prismic'
   ],
+
   prismic: {
     endpoint: 'https://my-site.cdn.prismic.io/api/v2',
     modern: !IS_DEV,
-    preview: IS_PREPROD ? '/preview/' : false
+    preview: IS_PREPROD ? '/preview/' : false,
+    apiOptions: {
+      routes: [
+        {
+          type: 'home_page',
+          path: '/'
+        },
+        {
+          type: 'project',
+          path: '/projects/:uid/'
+        }
+      ]
+    }
+
   }
 })
